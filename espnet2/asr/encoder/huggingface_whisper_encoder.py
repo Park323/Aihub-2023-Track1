97d92fe430e9bddb95c01a8fed38be89d132c8e2
Park323 <park32323@gmail.com>
2023-11-03 12:13:27 +0000

    inter ctc

--- espnet2/asr/encoder/huggingface_whisper_encoder.py
+++ espnet2/asr/encoder/huggingface_whisper_encoder.py
@@ -15,7 +15,9 @@ from espnet2.asr.encoder.abs_encoder import AbsEncoder
 
 
 class HuggingfaceWhisperEncoder(AbsEncoder):
-    def __init__(self, input_size: int = 80, output_size: int = 384, n_layer:int = 12, model_path: str="TheoJo/whisper-tiny-ko", freeze_weights: bool = False):
+    def __init__(self, input_size: int = 80, output_size: int = 384, n_layer:int = 12, 
+                 model_path: str="TheoJo/whisper-tiny-ko", freeze_weights: bool = False,
+                 interctc_layer_idx: List[int] = [], interctc_use_conditioning: bool = False):
         super().__init__()
         self.encoder = AutoModelForSpeechSeq2Seq.from_pretrained(model_path).model.encoder
         self.encoder.layers = self.encoder.layers[:n_layer]
@@ -24,6 +26,11 @@ class HuggingfaceWhisperEncoder(AbsEncoder):
         
         self.freeze_weights = freeze_weights
         if self.freeze_weights: logging.info(f"{type(self)} states are frozen.")
+        
+        self.interctc_layer_idx = interctc_layer_idx
+        if len(interctc_layer_idx) > 0:
+            assert 0 < min(interctc_layer_idx) and max(interctc_layer_idx) < n_layer
+        self.interctc_use_conditioning = interctc_use_conditioning
     
     def output_size(self) -> int:
         return self.output_size_
@@ -33,6 +40,7 @@ class HuggingfaceWhisperEncoder(AbsEncoder):
         xs_pad: torch.Tensor,
         ilens: torch.Tensor,
         prev_states: torch.Tensor = None,
+        ctc: CTC = None,
     ) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
         xs_pad = xs_pad.permute(0,2,1) # (B, 80, T)
         
@@ -52,43 +60,42 @@ class HuggingfaceWhisperEncoder(AbsEncoder):
             encoder_states = () if output_hidden_states else None
             all_attentions = () if output_attentions else None
 
-            for idx, encoder_layer in enumerate(self.encoder.layers):
+            intermediate_outs = []
+            for idx, encoder_layer in enumerate(self.encoder.layers, start=1):
                 if output_hidden_states:
                     encoder_states = encoder_states + (hidden_states,)
                 # add LayerDrop (see https://arxiv.org/abs/1909.11556 for description)
                 dropout_probability = random.uniform(0, 1)
-                if self.training and (dropout_probability < self.encoder.layerdrop):  # skip the layer
-                    layer_outputs = (None, None)
-                else:
-                    if self.encoder.gradient_checkpointing and self.training:
+                
+                layer_outputs = encoder_layer(
+                    hidden_states,
+                    None,
+                    layer_head_mask=None,
+                    output_attentions=output_attentions,
+                )
 
-                        def create_custom_forward(module):
-                            def custom_forward(*inputs):
-                                return module(*inputs, output_attentions)
+                hidden_states = layer_outputs[0]
+                
+                if idx in self.interctc_layer_idx:
+                    encoder_out = hidden_states
 
-                            return custom_forward
+                    # intermediate outputs are also normalized
+                    encoder_out = self.encoder.layer_norm(encoder_out)
 
-                        layer_outputs = torch.utils.checkpoint.checkpoint(
-                            create_custom_forward(encoder_layer),
-                            hidden_states,
-                            None,
-                            None,
-                        )
-                    else:
-                        layer_outputs = encoder_layer(
-                            hidden_states,
-                            None,
-                            layer_head_mask=None,
-                            output_attentions=output_attentions,
-                        )
+                    intermediate_outs.append((idx, encoder_out))
 
-                    hidden_states = layer_outputs[0]
+                    # if self.interctc_use_conditioning:
+                    #     ctc_out = ctc.softmax(encoder_out)
+
+                    #     xs_pad = xs_pad + self.conditioning_layer(ctc_out)
 
                 if output_attentions:
                     all_attentions = all_attentions + (layer_outputs[1],)
 
         hidden_states = self.encoder.layer_norm(hidden_states)
 
-        ilens = (ilens / 2).ceil().to(int)
+        olens = (ilens / 2).ceil().to(int)
 
-        return hidden_states, ilens, None
+        if intermediate_outs:
+            return (hidden_states, intermediate_outs), olens, None
+        return hidden_states, olens, None
